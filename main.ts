de24f1dfa2782f97cfc69130e87ea13baeb93c06
GuidoBergmans <75182919+GuidoBergmans@users.noreply.github.com>
2021-01-24 19:27:12 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,12 +8,11 @@ function fabriek () {
     tiles.setTilemap(tilemap`level4`)
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile43`, function (sprite, location) {
-    score += 10
+    info.changeScoreBy(10)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile41`, function (sprite, location) {
     wereld()
 })
-let score = 0
 let mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
