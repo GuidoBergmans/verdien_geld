e352d34a99609d5951989d1de3c4454aa37d462e
GuidoBergmans <75182919+GuidoBergmans@users.noreply.github.com>
2021-01-29 12:38:26 +0100

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -21,6 +21,60 @@ namespace myImages {
 . . . . . f f f f f f . . . . . 
 . . . . . f f . . f f . . . . . 
 `;
+            case "image2":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f 2 1 2 
+. . e 4 f 2 2 2 2 2 2 f 4 2 1 2 
+. . 4 d f 2 2 2 2 2 2 f d 2 1 2 
+. . 4 4 f 4 4 5 5 4 4 f 4 2 1 2 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image3":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f 2 1 2 
+. . e 4 f 2 2 2 2 2 2 f 4 2 1 2 
+. . 4 d f 2 2 2 2 2 2 f d 2 1 2 
+. . 4 4 f 4 4 5 5 4 4 f 4 2 1 2 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image4":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f 2 1 2 
+. . e 4 f 2 2 2 2 2 2 f 4 2 1 2 
+. . 4 d f 2 2 2 2 2 2 f d 2 1 2 
+. . 4 4 f 4 4 5 5 4 4 f 4 2 1 2 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -14,6 +14,25 @@ function fabriek () {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile32`, function (sprite, location) {
     info.changeScoreBy(-1)
     tiles.setTileAt(location, assets.tile`tile45`)
+    mySprite.destroy()
+    mySprite2 = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f 2 1 2 
+        . . e 4 f 2 2 2 2 2 2 f 4 2 1 2 
+        . . 4 d f 2 2 2 2 2 2 f d 2 1 2 
+        . . 4 4 f 4 4 5 5 4 4 f 4 2 1 2 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile43`, function (sprite, location) {
     info.changeScoreBy(10)
@@ -23,7 +42,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`tile43`, function (sprite, lo
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile41`, function (sprite, location) {
     wereld()
 })
-let mySprite = sprites.create(img`
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -48,6 +69,6 @@ forever(function () {
     controller.moveSprite(mySprite)
 })
 forever(function () {
-    music.setVolume(30)
+    music.setVolume(0)
     music.playMelody("G B A G C5 B A B ", 120)
 })
